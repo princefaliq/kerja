580e4770c5c36cc7553ffe13383567fd18a35be4
Achmad Faliqul Isbah <56667682+princefaliq@users.noreply.github.com>
2025-10-31 21:15:36 +0700

    Update jadwal.blade.php
    edit di rumah

--- resources/views/crafto/inc/jadwal.blade.php
+++ resources/views/crafto/inc/jadwal.blade.php
@@ -7,12 +7,12 @@
                 <div class="ps-20 pe-20 pt-25 pb-25 xxl-ps-15 xxl-pe-15 lg-p-7 sm-p-40px overflow-hidden h-100 text-center text-xl-start border-top border-end border-color-transparent-white-light">
                     <h2 class="alt-font fw-500 text-white ls-minus-2px"><span class="w-20px h-4px d-inline-block bg-base-color me-10px"></span>Jadwal event</h2>
                     <p class="mb-35px lg-w-50 md-w-70 sm-w-100 mx-auto mx-xl-auto">Jadwal event dapat berubah sesuai kondisi terkini. Mohon pantau informasi terbaru!</p>
-                    <a href="demo-conference-schedule.html" class="btn btn-large btn-dark-gray btn-hover-animation btn-round-edge btn-box-shadow align-self-center">
+                    {{-- <a href="demo-conference-schedule.html" class="btn btn-large btn-dark-gray btn-hover-animation btn-round-edge btn-box-shadow align-self-center">
                                 <span>
                                     <span class="btn-text">Download jadwal</span>
                                     <span class="btn-icon"><i class="fa-solid fa-arrow-right"></i></span>
                                 </span>
-                    </a>
+                    </a> --}}
                 </div>
             </div>
             <!-- end features box item -->
@@ -20,12 +20,17 @@
             <div class="col-xl-3 col-md-4 event-style-01 p-0">
                 <div class="bg-dark-midnight-blue hover-box will-change-inherit dark-hover feature-box ps-19 pe-19 pt-22 pb-27 md-p-8 md-pb-25 sm-pb-50px overflow-hidden h-100 text-center text-md-start border-top border-end border-color-transparent-white-light">
                     <div class="feature-box-content w-100 lg-mb-5 last-paragraph-no-margin">
-                        <div class="text-white fs-22 alt-font fw-500 mb-20px">Friday, Dec 24</div>
-                        <p class="text-light-opacity">Psychologist - John parker<br>10:00 AM to 12:30 PM</p>
+                        <div class="text-white fs-22 alt-font fw-500 mb-20px">Rabu, 12 Nov 25</div>
+                        <p class="text-light-opacity">JOB FAIR 2025
+                            <br>
+                            07:30 s/d 15:00
+                            <br>
+                            GOR PELITA, Jl. Letjen Sutarman
+                        </p>
                         <div class="divider-style-03 mb-20px divider-style-03-01 border-color-transparent-white-light"></div>
-                        <p class="text-light-opacity">Sociology - Herman miller<br>02:00 PM to 04:30 PM</p>
+                        {{-- <p class="text-light-opacity">Sociology - Herman miller<br>02:00 PM to 04:30 PM</p>
                         <div class="divider-style-03 mb-20px divider-style-03-01 border-color-transparent-white-light"></div>
-                        <p class="text-light-opacity">Geologist - Jeremy dupont<br>05:00 PM to 07:30 PM</p>
+                        <p class="text-light-opacity">Geologist - Jeremy dupont<br>05:00 PM to 07:30 PM</p> --}}
                         <span class="number fs-120 ls-minus-5px fw-500 text-outline text-outline-width-2px text-outline-color-base-color opacity-5 alt-font position-absolute bottom-minus-50px sm-bottom-minus-40px left-0px ps-20 sm-ps-0 sm-right-0px sm-left-0px">01</span>
                     </div>
                     <div class="feature-box-overlay bg-base-color"></div>
@@ -36,12 +41,12 @@
             <div class="col-xl-3 col-md-4 event-style-01 p-0">
                 <div class="bg-dark-midnight-blue hover-box will-change-inherit dark-hover feature-box ps-19 pe-19 pt-22 pb-27 md-p-8 md-pb-25 sm-pb-50px overflow-hidden h-100 text-center text-md-start border-top border-end border-color-transparent-white-light">
                     <div class="feature-box-content w-100 lg-mb-5 last-paragraph-no-margin">
-                        <div class="text-white fs-22 alt-font fw-500 mb-20px">Saturday, Dec 25</div>
+                        {{-- <div class="text-white fs-22 alt-font fw-500 mb-20px">Saturday, Dec 25</div>
                         <p class="text-light-opacity">Economy - Michal ruheen<br>10:00 AM to 12:30 PM</p>
                         <div class="divider-style-03 mb-20px divider-style-03-01 border-color-transparent-white-light"></div>
                         <p class="text-light-opacity">Engineer - Jessica dover<br>02:00 PM to 04:30 PM</p>
                         <div class="divider-style-03 mb-20px divider-style-03-01 border-color-transparent-white-light"></div>
-                        <p class="text-light-opacity">Psychologist - John parker<br>05:00 PM to 07:30 PM</p>
+                        <p class="text-light-opacity">Psychologist - John parker<br>05:00 PM to 07:30 PM</p> --}}
                         <span class="number fs-120 ls-minus-5px fw-500 text-outline text-outline-width-2px text-outline-color-base-color opacity-5 alt-font position-absolute bottom-minus-50px sm-bottom-minus-40px left-0px ps-20 sm-ps-0 sm-right-0px sm-left-0px">02</span>
                     </div>
                     <div class="feature-box-overlay bg-base-color"></div>
@@ -52,12 +57,12 @@
             <div class="col-xl-3 col-md-4 event-style-01 p-0">
                 <div class="bg-dark-midnight-blue hover-box will-change-inherit dark-hover feature-box ps-19 pe-19 pt-22 pb-27 md-p-8 md-pb-25 sm-pb-50px overflow-hidden h-100 text-center text-md-start border-top border-end border-color-transparent-white-light">
                     <div class="feature-box-content w-100 lg-mb-5 last-paragraph-no-margin">
-                        <div class="text-white fs-22 alt-font fw-500 mb-20px">Sunday, Dec 26</div>
+                        {{-- <div class="text-white fs-22 alt-font fw-500 mb-20px">Sunday, Dec 26</div>
                         <p class="text-light-opacity">Biologist- Saleena fountain<br>10:00 AM to 12:30 PM</p>
                         <div class="divider-style-03 mb-20px divider-style-03-01 border-color-transparent-white-light"></div>
                         <p class="text-light-opacity">Secretary- Paulina morris<br>02:00 PM to 04:30 PM</p>
                         <div class="divider-style-03 mb-20px divider-style-03-01 border-color-transparent-white-light"></div>
-                        <p class="text-light-opacity">Politician - Wendaya royin<br>05:00 PM to 07:30 PM</p>
+                        <p class="text-light-opacity">Politician - Wendaya royin<br>05:00 PM to 07:30 PM</p> --}}
                         <span class="number fs-120 ls-minus-5px fw-500 text-outline text-outline-width-2px text-outline-color-base-color opacity-5 alt-font position-absolute bottom-minus-50px sm-bottom-minus-40px left-0px ps-20 sm-ps-0 sm-right-0px sm-left-0px">03</span>
                     </div>
                     <div class="feature-box-overlay bg-base-color"></div>
